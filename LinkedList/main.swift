d9ab542ff111c5dec9ce2f29fb01244509764d79
Mitul Manish <mitul.manish@gmail.com>
2016-07-23 17:58:30 +1000

    Initial Commit

---
+++ LinkedList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LinkedList
+//
+//  Created by Mitul Manish on 23/07/2016.
+//  Copyright © 2016 Mitul Manish. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
